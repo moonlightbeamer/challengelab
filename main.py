18335ef8bd7e4fa4b54094b1364b4f1f633c9be9
Lin Zhu <moonlightbeamer@ymail.com>
2024-05-27 17:32:18 -0600

    remove lab 1 code

--- main.py
+++ main.py
@@ -70,91 +70,91 @@ embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@002")
 # Instantiate a Generative AI model here
 gen_model = GenerativeModel("gemini-1.5-flash-001")
 
-# Open the PDF file
-pdf_file = open(SOURCE_FILE_NAME, 'rb')
+# # Open the PDF file
+# pdf_file = open(SOURCE_FILE_NAME, 'rb')
 
-# Create a PDF reader object
-pdf_reader = pypdf.PdfReader(pdf_file)
+# # Create a PDF reader object
+# pdf_reader = pypdf.PdfReader(pdf_file)
 
-# Get the number of pages in the PDF
-num_pages = len(pdf_reader.pages)
-print(f"Read PDF with number of pages: {num_pages}")
+# # Get the number of pages in the PDF
+# num_pages = len(pdf_reader.pages)
+# print(f"Read PDF with number of pages: {num_pages}")
 
-with open(JSONL_FILE_NAME, 'w', encoding='utf-8') as f:
-  # Loop through each page
-  for page_num in range(num_pages):
+# with open(JSONL_FILE_NAME, 'w', encoding='utf-8') as f:
+#   # Loop through each page
+#   for page_num in range(num_pages):
 
-    # Get the current page
-    page = pdf_reader.pages[page_num]
+#     # Get the current page
+#     page = pdf_reader.pages[page_num]
       
-    # Extract the text from the page
-    text = page.extract_text()
+#     # Extract the text from the page
+#     text = page.extract_text()
     
-    # Write to Firestore
-    doc_ref = firestore_db.collection("page_content").document(str(page_num + 1))
-    doc_ref.set({'content': text})
-    # print(f"Document: {str(page_num + 1)} created in Firestore database successfully.")
+#     # Write to Firestore
+#     doc_ref = firestore_db.collection("page_content").document(str(page_num + 1))
+#     doc_ref.set({'content': text})
+#     # print(f"Document: {str(page_num + 1)} created in Firestore database successfully.")
 
-    # Get the embeddings for the text
-    raw_embeddings_with_metadata = embedding_model.get_embeddings([text])
-    embeddings = [embedding.values for embedding in raw_embeddings_with_metadata][0]
+#     # Get the embeddings for the text
+#     raw_embeddings_with_metadata = embedding_model.get_embeddings([text])
+#     embeddings = [embedding.values for embedding in raw_embeddings_with_metadata][0]
 
-    # construct Panda dataframe
-    data = {
-      "id": str(page_num + 1),
-      "embedding": embeddings
-    }
+#     # construct Panda dataframe
+#     data = {
+#       "id": str(page_num + 1),
+#       "embedding": embeddings
+#     }
 
-    # Write to jsonl file
-    f.write(json.dumps(data) + "\n")
+#     # Write to jsonl file
+#     f.write(json.dumps(data) + "\n")
 
-    # print(f"Embedding: {str(page_num + 1)} saved in JSONL file successfully.")
+#     # print(f"Embedding: {str(page_num + 1)} saved in JSONL file successfully.")
 
-# Close the PDF file
-pdf_file.close()
+# # Close the PDF file
+# pdf_file.close()
 
-# Create a GCS bucket to store the JSONL file
-client = storage.Client(project=PROJECT_ID)
-bucket = client.bucket(BUCKET_NAME)
+# # Create a GCS bucket to store the JSONL file
+# client = storage.Client(project=PROJECT_ID)
+# bucket = client.bucket(BUCKET_NAME)
 
-# Create the bucket if it doesn't exist
-try:
-  bucket = client.create_bucket(bucket, location=LOCATION)
-  # print(f"Bucket {BUCKET_NAME} created successfully.")
-except Exception as e:
-  print(f"Error creating bucket: {e}")
+# # Create the bucket if it doesn't exist
+# try:
+#   bucket = client.create_bucket(bucket, location=LOCATION)
+#   # print(f"Bucket {BUCKET_NAME} created successfully.")
+# except Exception as e:
+#   print(f"Error creating bucket: {e}")
 
-# Upload the file to the bucket
-blob = bucket.blob(JSONL_FILE_NAME)
-blob.upload_from_filename(JSONL_FILE_NAME)
-print(f"File {JSONL_FILE_NAME} uploaded to bucket {BUCKET_NAME}.")
+# # Upload the file to the bucket
+# blob = bucket.blob(JSONL_FILE_NAME)
+# blob.upload_from_filename(JSONL_FILE_NAME)
+# print(f"File {JSONL_FILE_NAME} uploaded to bucket {BUCKET_NAME}.")
 
-# Create a vector index from the JSON-L file
-index = aiplatform.MatchingEngineIndex.create_tree_ah_index(
-  display_name = INDEX_NAME,
-  contents_delta_uri = JSONL_FILE_PATH,
-  dimensions = INDEX_DIMENSION,
-  approximate_neighbors_count = INDEX_APPROXIMATE_NEIGHBORS_COUNT,
-)
+# # Create a vector index from the JSON-L file
+# index = aiplatform.MatchingEngineIndex.create_tree_ah_index(
+#   display_name = INDEX_NAME,
+#   contents_delta_uri = JSONL_FILE_PATH,
+#   dimensions = INDEX_DIMENSION,
+#   approximate_neighbors_count = INDEX_APPROXIMATE_NEIGHBORS_COUNT,
+# )
 
-# Wait for the index creation to complete
-index.wait()
+# # Wait for the index creation to complete
+# index.wait()
 
-# print(f"Vector index created: {index.name}")
+# # print(f"Vector index created: {index.name}")
 
-# Deploy the vector index as an endpoint
-endpoint = aiplatform.MatchingEngineIndexEndpoint.create(
-  display_name = INDEX_ENDPOINT_NAME,
-  public_endpoint_enabled = True,
-  description="Assessment Index Endpoint",
-)
+# # Deploy the vector index as an endpoint
+# endpoint = aiplatform.MatchingEngineIndexEndpoint.create(
+#   display_name = INDEX_ENDPOINT_NAME,
+#   public_endpoint_enabled = True,
+#   description="Assessment Index Endpoint",
+# )
 
-endpoint = endpoint.deploy_index(
-  index = index, 
-  deployed_index_id = DEPLOYED_INDEX_ID,
-)
+# endpoint = endpoint.deploy_index(
+#   index = index, 
+#   deployed_index_id = DEPLOYED_INDEX_ID,
+# )
 
-# print(f"Endpoint deployed: {endpoint.resource_name}")
+# # print(f"Endpoint deployed: {endpoint.resource_name}")
 
 app = Flask(__name__)
 
@@ -166,7 +166,7 @@ def main():
     # They haven't yet submitted the form
     if request.method == 'GET':
         question = ""
-        answer = "Hi, I'm FreshBot, what can I do for you?"
+        answer = "Hi, I'm FreshBot, what can I do for you today?"
 
     # The user asked a question and submitted the form
     # The request.method would equal 'POST'
