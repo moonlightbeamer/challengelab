aa418c1f1778150c2331c3d593bbd866ee0c316f
Lin Zhu <moonlightbeamer@ymail.com>
2024-05-27 23:43:28 -0600

    add lab 1 code

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ from vertexai.language_models import (
     TextEmbeddingModel,
 )
 
+import pypdf
+import json
+from google.cloud import aiplatform
+from google.cloud import storage
+
 PROJECT_ID = os.getenv('PROJECT_ID')
 LOCATION = os.getenv('LOCATION')
 GEN_MODEL_ID = "gemini-1.5-flash-001"
@@ -83,6 +88,105 @@ safety_settings = {
     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
 }
 
+# RAG variables definition
+SOURCE_FILE_NAME = "fpc-manual.pdf"
+JSONL_FILE_NAME = "embeddings.json"
+BUCKET_NAME = "{PROJECT_ID}-challengelab"
+JSONL_FILE_PATH = f"gs://{BUCKET_NAME}"
+INDEX_DIMENSION = 768
+INDEX_APPROXIMATE_NEIGHBORS_COUNT = 5
+INDEX_NAME = "assessment-index"
+DEPLOYED_INDEX_ID = "assessment_index_deployed"
+INDEX_ENDPOINT_NAME = "assessment-index-endpoint"
+# INDEX_MACHINE_TYPE = "n1-standard-2"
+# generate an unique id for this session
+from datetime import datetime
+UID = datetime.now().strftime("%m%d%H%M")
+
+# Open the PDF file
+pdf_file = open(SOURCE_FILE_NAME, 'rb')
+
+# Create a PDF reader object
+pdf_reader = pypdf.PdfReader(pdf_file)
+
+# Get the number of pages in the PDF
+num_pages = len(pdf_reader.pages)
+# print(f"Read PDF with number of pages: {num_pages}")
+
+with open(JSONL_FILE_NAME, 'w', encoding='utf-8') as f:
+  # Loop through each page
+  for page_num in range(num_pages):
+
+    # Get the current page
+    page = pdf_reader.pages[page_num]
+      
+    # Extract the text from the page
+    text = page.extract_text()
+    
+    # Write to Firestore
+    doc_ref = db.collection("page_content").document(str(page_num + 1))
+    doc_ref.set({'content': text})
+    # print(f"Document: {str(page_num + 1)} created in Firestore database successfully.")
+
+    # Get the embeddings for the text
+    raw_embeddings_with_metadata = embedding_model.get_embeddings([text])
+    embeddings = [embedding.values for embedding in raw_embeddings_with_metadata][0]
+
+    # construct Panda dataframe
+    data = {
+      "id": str(page_num + 1),
+      "embedding": embeddings
+    }
+
+    # Write to jsonl file
+    f.write(json.dumps(data) + "\n")
+
+    # print(f"Embedding: {str(page_num + 1)} saved in JSONL file successfully.")
+
+# Close the PDF file
+pdf_file.close()
+
+# Create a GCS bucket to store the JSONL file
+client = storage.Client(project=PROJECT_ID)
+bucket = client.bucket(BUCKET_NAME)
+
+# Create the bucket if it doesn't exist
+try:
+  bucket = client.create_bucket(bucket, location=LOCATION)
+  print(f"Bucket {BUCKET_NAME} created successfully.")
+except Exception as e:
+  print(f"Error creating bucket: {e}")
+
+# Upload the file to the bucket
+blob = bucket.blob(JSONL_FILE_NAME)
+blob.upload_from_filename(JSONL_FILE_NAME)
+# print(f"File {JSONL_FILE_NAME} uploaded to bucket {BUCKET_NAME}.")
+
+# Create a vector index from the JSON-L file
+index = aiplatform.MatchingEngineIndex.create_tree_ah_index(
+  display_name = INDEX_NAME,
+  contents_delta_uri = JSONL_FILE_PATH,
+  dimensions = INDEX_DIMENSION,
+  approximate_neighbors_count = INDEX_APPROXIMATE_NEIGHBORS_COUNT,
+)
+
+# Wait for the index creation to complete
+index.wait()
+
+# print(f"Vector index created: {index.name}")
+
+# Deploy the vector index as an endpoint
+endpoint = aiplatform.MatchingEngineIndexEndpoint.create(
+  display_name = INDEX_ENDPOINT_NAME,
+  public_endpoint_enabled = True,
+  description="Assessment Index Endpoint",
+)
+
+endpoint = endpoint.deploy_index(
+  index = index, 
+  deployed_index_id = DEPLOYED_INDEX_ID,
+)
+
 app = Flask(__name__)
 
 # The Home page route
@@ -121,6 +225,34 @@ def search_vector_database(question):
     # 3. Get the IDs for the five embeddings that are returned
     # 4. Get the five documents from Firestore that match the IDs
     # 5. Concatenate the documents into a single string and return it
+#     # 1. Convert the question into an embedding
+#     raw_embeddings_with_metadata = embedding_model.get_embeddings([question])
+#     embedding = [embedding.values for embedding in raw_embeddings_with_metadata][0]
+
+#     # 2. Search the Vector database for the 5 closest embeddings to the user's question
+#     search_results = vertexai.search_vectors(embedding, 5)
+#     search_vector_index(
+#     query_text=prompt_text,
+#     index_id="your-index-id",
+#     top_k=5,
+# )
+
+#     # 3. Get the IDs for the five embeddings that are returned
+#     ids = [result.id for result in search_results]
+
+#     # 4. Get the five documents from Firestore that match the IDs
+#     docs = db.collection("page_content").where(u'id', 'in', ids).stream()
+
+#     # 5. Concatenate the documents into a single string and return it
+#     data = ""
+#     for doc in docs:
+#         doc_data = doc.to_dict()
+#         doc_text = doc_data.get('content', '')  # the text content is stored in the 'content' field
+#         data += doc_text + ' '  # Append the document text to data
+
+#     # Remove the trailing space
+#     data = data.strip()
+#     return data
 
     data = """fo d P R O T E C T I O N T R A I N I N G M A N U A L
 N E W Y O R K C I T Y D E P A R T M E N T O F H E A L T H & M E N T A L H Y G I E N E
